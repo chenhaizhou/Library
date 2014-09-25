2019cc4b7eed6d6607ff9306c2999014ba305d97
summer <xiasiyu@gmail.com>
2014-09-25 19:26:20 +0800

    first controller and test

---
+++ src/main/java/com/thoughtworks/dolphin/controller/HelloController.java
@@ -0,0 +1,4 @@
+package com.thoughtworks.dolphin.controller;
+
+public class HelloController {
+}
---
+++ src/test/java/com/thoughtworks/dolphin/controller/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.dolphin.controller;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class HelloControllerTest {
+    @Test
+    public void shouldTest() {
+        assertThat(1, is(1));
+    }
+}
