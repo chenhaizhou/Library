d595a21f68dad7ad5fd773aef0f26b08de40a925
Joe <zhoujie338@126.com>
2014-10-10 15:07:48 +0800

    #add book: [Joe & chenhaizhou] - delete useless code

--- src/main/java/com/thoughtworks/dolphin/controller/HelloController.java
+++ src/main/java/com/thoughtworks/dolphin/controller/HelloController.java
@@ -9,24 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HelloController {
 
-    @Autowired
-    private UserService userService;
-
     @RequestMapping("index")
     public String index(){
         return "index";
-    }
-
-    @RequestMapping("list")
-         public String list()
-    {
-        return "list";
-    }
-
-    @RequestMapping("login")
-    public String login()
-    {
-        return "login";
     }
 
 }
