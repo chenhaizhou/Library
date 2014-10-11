625a18691c633a4f0ea665f9dd6a2a9f53fe56bb
chenhaizhou <hzchen@CNhzchen.local>
2014-10-11 10:21:36 +0800

    #search: [chenhaizhou & han] - add search form

--- src/main/webapp/assets/scripts/booklist.js
+++ src/main/webapp/assets/scripts/booklist.js
@@ -1,20 +1,29 @@
 function loadBookList(totalCount, itemCountInEachPage) {
     $('#smart-paginator').smartpaginator({ totalrecords: totalCount, recordsperpage: itemCountInEachPage, initval: 0, next: 'Next', prev: 'Prev', first: 'First', last: 'Last', theme: 'black', onchange: onChange
     });
-    loadBooksByPage(1);
+
+    var fliter = {
+        pageValue:1,
+        searchKey:''
+    };
+    loadBooksByPage(fliter);
 }
 
 function onChange(newPageValue) {
-    loadBooksByPage(newPageValue);
+    var fliter = {
+        pageValue:newPageValue,
+        searchKey:$('#searchKey').val()
+    };
+    loadBooksByPage(fliter);
 }
 
-function loadBooksByPage(pageValue) {
+function loadBooksByPage(fliter) {
     var template = $.templates("#bookTmpl");
     $.ajax({
         type: "POST",
         url: basePath + "/listBooks.do",
         contentType: "application/json; charset=utf-8",
-        data:"" + pageValue,
+        data:"" + fliter,
         dataType: 'json',
         success: function (result) {
             var htmlOutput = template.render(result);
@@ -40,5 +49,13 @@ $(document).ready(function() {
         error :function() {
             alert("wrong");
         }
+    });
+
+    $('#searchBtn').click(function(){
+        var fliter = {
+            pageValue:1,
+            searchKey:$('#searchKey').val()
+        };
+        loadBooksByPage(fliter)
     });
 });
