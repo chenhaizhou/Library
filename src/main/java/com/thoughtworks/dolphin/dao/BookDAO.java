83eb8a09ad10178ebd3863555013d57a932cc148
WuLingzi <lotuswlz@sina.com>
2014-10-14 17:22:51 +0800

    clean code

--- src/main/java/com/thoughtworks/dolphin/dao/BookDAO.java
+++ src/main/java/com/thoughtworks/dolphin/dao/BookDAO.java
@@ -2,10 +2,6 @@ package com.thoughtworks.dolphin.dao;
 
 import com.thoughtworks.dolphin.dto.BookSearchCondition;
 import com.thoughtworks.dolphin.model.Book;
-import com.thoughtworks.dolphin.model.Image;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Map;
