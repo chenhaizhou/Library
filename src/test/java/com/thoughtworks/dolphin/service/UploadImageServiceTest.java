cbbdaed824eeb1ed4a3ffd665394bf4b5c35f46f
Joe <zhoujie338@126.com>
2014-10-16 17:34:22 +0800

    #edit book: [Joe & Haidong Zhang] - add testcase about upload image

--- src/test/java/com/thoughtworks/dolphin/service/UploadImageServiceTest.java
+++ src/test/java/com/thoughtworks/dolphin/service/UploadImageServiceTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.dolphin.service;
 import com.thoughtworks.dolphin.AbstractUnitTest;
 import com.thoughtworks.dolphin.common.Constants;
 import com.thoughtworks.dolphin.dao.ImageDAO;
+import com.thoughtworks.dolphin.dto.BookQuery;
 import com.thoughtworks.dolphin.model.Image;
 import com.thoughtworks.dolphin.service.impl.UploadServiceImpl;
 import com.thoughtworks.dolphin.util.UploadImageUtil;
@@ -12,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.test.context.ContextConfiguration;
@@ -28,9 +30,10 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(UploadImageUtil.class)
+@PrepareForTest({UploadImageUtil.class, UploadServiceImpl.class})
 public class UploadImageServiceTest {
 
     @InjectMocks
@@ -65,6 +68,31 @@ public class UploadImageServiceTest {
         int result = uploadService.uploadFile(file, request);
 
         assertTrue(result == 0);
+
+    }
+
+    @Test
+    public void shouldUploadFileWhenSaveFileSuccess() throws Exception {
+
+        String realPath = "/home/zj/upload";
+        String contextPath = "Library";
+        String fileName = "xxxxxxxx_203023843243.jpg";
+
+        HttpServletRequest request = getHttpServletRequest(realPath, contextPath);
+
+        MultipartFile file = mock(MultipartFile.class);
+
+        when(UploadImageUtil.generateFileName(file)).thenReturn(fileName);
+        when(UploadImageUtil.saveFile2Disk(file, realPath, fileName)).thenReturn(true);
+
+        Image image = PowerMockito.mock(Image.class);
+
+        whenNew(Image.class).withAnyArguments().thenReturn(image);
+        when(image.getImageId()).thenReturn(100);
+
+        int result = uploadService.uploadFile(file, request);
+
+        assertTrue(result == 100);
 
     }
 
